b1776f307419aa89cee72e06d1f16086767acd33
khussa1n <khussain.qudaibergenov@gmail.com>
2023-08-10 23:03:35 +0600

    update custom_error.go

--- internal/custom_error/custom_error.go
+++ internal/custom_error/custom_error.go
@@ -3,6 +3,8 @@ package custom_error
 import "errors"
 
 var (
+	ErrEmptyID               = errors.New("empty id param")
+	ErrInvalidIDParameter    = errors.New("invalid id param")
 	ErrTaskNotFound          = errors.New("task not found")
 	ErrMessageTooLong        = errors.New("more than 200 char")
 	ErrInvalidActiveAtFormat = errors.New("activeAt invalid format")
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,8 @@
 package handler
 
 import (
-	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/khussa1n/todo-list/internal/custom_error"
 	"github.com/khussa1n/todo-list/internal/service"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -20,12 +20,12 @@ func New(srvs service.Service) *Handler {
 func parseIdFromPath(c *gin.Context, param string) (primitive.ObjectID, error) {
 	idParam := c.Param(param)
 	if idParam == "" {
-		return primitive.ObjectID{}, errors.New("empty id param")
+		return primitive.ObjectID{}, custom_error.ErrEmptyID
 	}
 
 	id, err := primitive.ObjectIDFromHex(idParam)
 	if err != nil {
-		return primitive.ObjectID{}, errors.New("invalid id param")
+		return primitive.ObjectID{}, custom_error.ErrInvalidIDParameter
 	}
 
 	return id, nil
